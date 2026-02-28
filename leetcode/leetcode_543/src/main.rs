c60bd381ab391967d5f9a538a6336902a5d28e0b
Ryan Fu <z5451018@ad.unsw.edu.au>
2026-02-28 11:38:05 +0800

    Fix writing style of leetcode_543

--- leetcode/leetcode_543/src/main.rs
+++ leetcode/leetcode_543/src/main.rs
@@ -1,29 +1,30 @@
 // Definition for a binary tree node.
 #[derive(Debug, PartialEq, Eq)]
 pub struct TreeNode {
-  pub val: i32,
-  pub left: Option<Rc<RefCell<TreeNode>>>,
-  pub right: Option<Rc<RefCell<TreeNode>>>,
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
 }
 
 impl TreeNode {
-  #[inline]
-  pub fn new(val: i32) -> Self {
-    TreeNode {
-      val,
-      left: None,
-      right: None
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
     }
-  }
 }
 
 struct Solution {}
 
-use std::rc::Rc;
 use std::cell::RefCell;
+use std::rc::Rc;
 impl Solution {
     pub fn diameter_of_binary_tree(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
         let mut max = 0;
+
         fn calculate_depth(root: Option<Rc<RefCell<TreeNode>>>, max: &mut i32) -> i32 {
             if let Some(node_rc) = root {
                 let (left, right) = {
@@ -35,12 +36,12 @@ impl Solution {
                 if (left_depth + right_depth) > *max {
                     *max = left_depth + right_depth;
                 }
-                return i32::max(left_depth, right_depth) + 1;
+                i32::max(left_depth, right_depth) + 1
             } else {
-                return 0;
+                0
             }
-
         }
+
         calculate_depth(root, &mut max);
         max
     }
